2939dd1dff943d1455e10b9ba6f4613d5052798e
Michael Limb <something15525@gmail.com>
2016-06-05 06:12:11 +0000

    Add post method placeholder for LEDS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var http = require('http');
+var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+var port = parseInt(process.env.PORT, 10) || 8080;
+
 var inputs = [{ pin: '11', gpio: '17', value: 1 },
               { pin: '12', gpio: '18', value: 0 }];
-
-app.use(express['static'](__dirname));
 
 // Express route for incoming requests for a customer name
 app.get('/inputs/:id', function(req, res) {
@@ -18,6 +20,11 @@ app.get('*', function(req, res) {
   res.status(404).send('Unrecognised API call');
 });
 
+app.put('/leds', function(req, res) {
+  res.send('Got the following req: ' + JSON.stringify(req.body));
+  console.log(JSON.stringify(req.body));
+});
+
 // Express route to handle errors
 app.use(function(err, req, res, next) {
   if (req.xhr) {
@@ -27,5 +34,5 @@ app.use(function(err, req, res, next) {
   }
 });
 
-app.listen(3000);
-console.log('App Server running at port 3000');
+app.listen(port);
+console.log('App Server running at port ' + port);
