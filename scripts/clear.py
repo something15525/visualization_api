295d13e15a9b45ac86976a4a73907b0821a35cde
Michael Limb <something15525@gmail.com>
2016-07-04 23:01:39 -0700

    Add license

--- scripts/clear.py
+++ scripts/clear.py
@@ -1,8 +1,18 @@
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
+# Copyright 2016 Michael Limb (something15525@gmail.com), Tony DiCola (tony@tonydicola.com)
 #
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import time
 
 from neopixel import *
@@ -21,10 +31,10 @@ if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
-	
+
 	colors = []
-	
+
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, Color(0, 0, 0))
-		
+
 	strip.show()
--- scripts/solid.py
+++ scripts/solid.py
@@ -1,5 +1,16 @@
-# Solid color script
-# Author: Michael Limb (somerthing15525@gmail.com)
+# Copyright 2016 Michael Limb (something15525@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Used to set a strand of Neopixels a certain color.
 import time
@@ -31,7 +42,7 @@ if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
-	
+
 	colors = []
 
 	# Set each led in the range of pixels
@@ -41,6 +52,6 @@ if __name__ == '__main__':
 			strip.setPixelColor(i, colorToSet)
 		else:
 			strip.setPixelColor(i, Color(25, 25, 112))
-	
-	# Show result	
+
+	# Show result
 	strip.show()
